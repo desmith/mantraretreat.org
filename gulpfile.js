7675eb3558223163e8c918e617ad54e858013d00
Dave E Smith <desmith@gmail.com>
2019-07-31 23:28:41 -0400

    updates costs

--- gulpfile.js
+++ gulpfile.js
@@ -17,7 +17,7 @@ var banner = ['/*!\n',
 ].join('');
 
 // Copy third party libraries from /node_modules into /vendor
-gulp.task('vendor', function() {
+gulp.task('vendor', (done) => {
 
   // Bootstrap
   gulp.src([
@@ -62,10 +62,11 @@ gulp.task('vendor', function() {
     ])
     .pipe(gulp.dest('./vendor/scrollreveal'))
 
+  done();
 });
 
 // Compile SCSS
-gulp.task('css:compile', function() {
+gulp.task('css:compile', () => {
   return gulp.src('./scss/**/*.scss')
     .pipe(sass.sync({
       outputStyle: 'expanded'
@@ -74,7 +75,7 @@ gulp.task('css:compile', function() {
 });
 
 // Minify CSS
-gulp.task('css:minify', ['css:compile'], function() {
+gulp.task('css:minify', gulp.series('css:compile', () => {
   return gulp.src([
       './css/*.css',
       '!./css/*.min.css'
@@ -85,13 +86,13 @@ gulp.task('css:minify', ['css:compile'], function() {
     }))
     .pipe(gulp.dest('./css'))
     .pipe(browserSync.stream());
-});
+}));
 
 // CSS
-gulp.task('css', ['css:compile', 'css:minify']);
+gulp.task('css', gulp.series('css:compile', 'css:minify'));
 
 // Minify JavaScript
-gulp.task('js:minify', function() {
+gulp.task('js:minify', () => {
   return gulp.src([
       './js/*.js',
       '!./js/*.min.js'
@@ -105,13 +106,13 @@ gulp.task('js:minify', function() {
 });
 
 // JS
-gulp.task('js', ['js:minify']);
+gulp.task('js', gulp.series('js:minify'));
 
 // Default task
-gulp.task('default', ['css', 'js', 'vendor']);
+gulp.task('default', gulp.series('css', 'js', 'vendor'));
 
 // Configure the browserSync task
-gulp.task('browserSync', function() {
+gulp.task('browserSync', () => {
   browserSync.init({
     server: {
       baseDir: "./"
@@ -120,8 +121,8 @@ gulp.task('browserSync', function() {
 });
 
 // Dev task
-gulp.task('dev', ['css', 'js', 'browserSync'], function() {
+gulp.task('dev', gulp.series('css', 'js', 'browserSync', () => {
   gulp.watch('./scss/*.scss', ['css']);
   gulp.watch('./js/*.js', ['js']);
   gulp.watch('./*.html', browserSync.reload);
-});
+}));
--- js/mantraretreat.js
+++ js/mantraretreat.js
@@ -1,46 +1,46 @@
-var hasPkg = function(step) {
+var hasPkg = function(step, link) {
 
-  var reg_link = "";
-  var registration_value = "";
-  if (step == 'register') {
-    reg_link = 'https://docs.google.com/forms/d/e/1FAIpQLScfVLqNJ7maXR0dXMTQCo2MTh12xFX_AQZwmDd9lYdpabF0eg/viewform?usp=pp_url';
-  } else if (step == 'paypal') {
-    reg_link = 'https://paypal.me/ISKCONCommunications';
-  }
+    var reg_link = "";
+    var registration_value = "";
+    if (step == 'register') {
+        reg_link = 'https://forms.gle/6JacDZZGb26msEpm8?usp=pp_url';
+    } else if (step == 'paypal') {
+        reg_link = 'https://paypal.me/ISKCONCommunications';
+    }
 
-  console.log('hasPkg('+step+')');
+  console.log('hasPkg(' + step + ')');
 
-  var radios = document.getElementsByName('reg_amt');
-  for (var i = 0, length = radios.length; i < length; i++) {
-    if (radios[i].checked) {
+    var radios = document.getElementsByName('reg_amt');
+    for (var i = 0, length = radios.length; i < length; i++) {
+        if (radios[i].checked) {
 
-      registration_value = radios[i].value;
-      str_value = registration_value.split(':')[1];
-      //document.getElementById('os0').value =
-      document.forms['registration_pkg_paypal'].elements['os0'].selectedIndex = i+1;
-      console.log('value: ' + registration_value );
+        registration_value = radios[i].value;
+        str_value = registration_value.split(':')[1];
+        document.forms['registration_pkg_paypal'].elements['os0'].selectedIndex = i+1;
+        console.log('value: ' + registration_value );
 
-      if (step == 'register') {
-        str_RegPkg = '$' + registration_value
-        reg_link += '&entry.371804978=' + str_RegPkg;
-        window.open(reg_link);
-      }
+        if (step == 'register') {
+            str_RegPkg = '$' + registration_value
+            reg_link += '&entry.371804978=' + str_RegPkg;
+            window.open(reg_link);
+        }
 
-      if (step == "paypal") {
-        reg_link += '/' + registration_value.split(':')[0];
-      }
+        if (step == "paypal") {
+            reg_link += '/' + registration_value.split(':')[0];
+        }
 
-      console.log('reg_link: ' + reg_link);
+        console.log('reg_link: ' + reg_link);
 
-      // only one radio can be logically checked, don't check the rest
-      break;
-     } //end if(radio[i].checked)
+        // only one radio can be logically checked, don't check the rest
+        break;
+       } //end if(radio[i].checked)
 
     } //end for loop
 
-  if (registration_value == "") {
-    alert("Please select a registration Package.");
-  }
+    if (registration_value == "") {
+        alert("Please select a registration Package.");
+        return false;
+    }
 
-  return false;
+    return true;
 }
