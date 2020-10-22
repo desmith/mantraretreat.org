6fd1b7470930414d15b57d3e21dba0b12ed22cb5
Dave Smith <dsmith@seizuretracker.com>
2020-10-21 23:06:38 -0400

    addedd donation options, linked to Paypal and Google Forms

--- js/mantraretreat.js
+++ js/mantraretreat.js
@@ -1,47 +1,40 @@
-var hasPkg = function(step, link) {
 
-    var reg_link = "";
-    var registration_value = "";
-    if (step == 'register') {
-        reg_link = 'https://docs.google.com/forms/d/e/1FAIpQLSfslRbnvdF-RYDXy2xO7Cqy_GgY1OpIKWM-I9CzxfNzPtse7w/viewform?usp=pp_url';
-    } else if (step == 'paypal') {
-        reg_link = 'https://paypal.me/ISKCONCommunications';
-    }
+var donation_selected = false;
+var donation_amount = 0;
 
-  console.log('hasPkg(' + step + ')');
+var chkFrm = function(form, action) {
 
-    var radios = document.getElementsByName('reg_amt');
-    for (var i = 0, length = radios.length; i < length; i++) {
+    var pp_link =  'https://paypal.me/ISKCONCommunications/';
+    var radios = form['reg_amt'];
+    for (i = 0; i < radios.length; i++) {
         if (radios[i].checked) {
-
-        registration_value = radios[i].value;
-        str_value = registration_value.split(':')[1];
-        document.forms['registration_pkg_paypal'].elements['os0'].selectedIndex = i+1;
-        console.log('value: ' + registration_value );
-
-        if (step == 'register') {
-            str_RegPkg = '$' + registration_value
-            uri = '&entry.371804978=' + str_RegPkg;
-            reg_link += encodeURI(uri);
-            window.open(reg_link);
+            donation_selected = true;
+            donation_amount = radios[i].value;
+            if (donation_amount < 0) {
+                $('#pp_button').hide();
+            } else {
+                pp_link += donation_amount;
+                $('#pp_button').show();
+            }
         }
-
-        if (step == "paypal") {
-            reg_link += '/' + registration_value.split(':')[0];
-        }
-
-        console.log('reg_link: ' + reg_link);
-
-        // only one radio can be logically checked, don't check the rest
-        break;
-       } //end if(radio[i].checked)
-
-    } //end for loop
-
-    if (registration_value == "") {
-        alert("Please select a registration Package.");
-        return false;
     }
 
-    return true;
+    if (action == 'submit') {
+        window.open(pp_link, '_blank');
+    }
+    return false;
+}
+
+var stepTwo = function () {
+    var reg_link = 'https://docs.google.com/forms/d/e/1FAIpQLScaucbXHE-YRdjKmbgaLpfpXpzLw45wzzNpeRwco-sMe_vr6A/viewform?usp=pp_url';
+    if (!donation_selected) {
+        alert("Please select a registration Package.");
+    } else {
+
+        var params = '&entry.371804978=$' + donation_amount + '+USD';
+        reg_link += encodeURI(params);
+        window.open(reg_link);
+
+    }
+    return false
 }
