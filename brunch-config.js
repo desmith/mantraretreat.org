493632ea7ed3178438cd296b742a07e654045292
Dave Smith <dsmith@seizuretracker.com>
2020-10-22 15:01:42 -0400

    brunch build is working, now for the imports...

---
+++ brunch-config.js
@@ -0,0 +1,64 @@
+module.exports = {
+    npm: {
+        globals: {
+            $: 'jquery',
+            jQuery: 'jquery',
+            bootstrap: 'bootstrap'
+        },
+        styles: {
+            bootstrap: ['scss/bootstrap.scss']
+        },
+    },
+
+    conventions: {
+        assets: /assets\//,
+    },
+
+    files: {
+        javascripts: {
+            joinTo: {
+                'js/mantraretreat.js': /^(src\/js)/,
+                'js/vendor.js': /node_modules/,
+            },
+            order: {
+                before: [
+                    'node_modules/jquery/src/jquery.js',
+                    'node_modules/bootstrap/dist/js/bootstrap.min.js'
+                ]
+            },
+        },
+        stylesheets: {
+            joinTo: {
+                'css/mantraretreat.css': /^(src\/styl)/,
+                'css/creative.css': /^(src\/scss\/creative\.scss)|(src\/css\/creative\.css)/,
+            },
+        },
+    },
+
+    modules: {
+        nameCleaner: path => path.replace(/^src\//, ''),
+    },
+
+    overrides: {
+        production: {
+          sourceMaps: true
+        }
+    },
+
+    paths: {
+        // Where to compile files to
+        public: 'static',
+
+        watched: [
+           'src',
+        ],
+    },
+
+    plugins: {
+      babel: {
+        // Do not use ES6 compiler in vendor code
+        //ignore: [/web\/static\/vendor/],
+      }
+    },
+
+}
---
+++ static/js/mantraretreat.js
@@ -0,0 +1,306 @@
+(function() {
+  'use strict';
+
+  var globals = typeof global === 'undefined' ? self : global;
+  if (typeof globals.require === 'function') return;
+
+  var modules = {};
+  var cache = {};
+  var aliases = {};
+  var has = {}.hasOwnProperty;
+
+  var expRe = /^\.\.?(\/|$)/;
+  var expand = function(root, name) {
+    var results = [], part;
+    var parts = (expRe.test(name) ? root + '/' + name : name).split('/');
+    for (var i = 0, length = parts.length; i < length; i++) {
+      part = parts[i];
+      if (part === '..') {
+        results.pop();
+      } else if (part !== '.' && part !== '') {
+        results.push(part);
+      }
+    }
+    return results.join('/');
+  };
+
+  var dirname = function(path) {
+    return path.split('/').slice(0, -1).join('/');
+  };
+
+  var localRequire = function(path) {
+    return function expanded(name) {
+      var absolute = expand(dirname(path), name);
+      return globals.require(absolute, path);
+    };
+  };
+
+  var initModule = function(name, definition) {
+    var hot = hmr && hmr.createHot(name);
+    var module = {id: name, exports: {}, hot: hot};
+    cache[name] = module;
+    definition(module.exports, localRequire(name), module);
+    return module.exports;
+  };
+
+  var expandAlias = function(name) {
+    var val = aliases[name];
+    return (val && name !== val) ? expandAlias(val) : name;
+  };
+
+  var _resolve = function(name, dep) {
+    return expandAlias(expand(dirname(name), dep));
+  };
+
+  var require = function(name, loaderPath) {
+    if (loaderPath == null) loaderPath = '/';
+    var path = expandAlias(name);
+
+    if (has.call(cache, path)) return cache[path].exports;
+    if (has.call(modules, path)) return initModule(path, modules[path]);
+
+    throw new Error("Cannot find module '" + name + "' from '" + loaderPath + "'");
+  };
+
+  require.alias = function(from, to) {
+    aliases[to] = from;
+  };
+
+  var extRe = /\.[^.\/]+$/;
+  var indexRe = /\/index(\.[^\/]+)?$/;
+  var addExtensions = function(bundle) {
+    if (extRe.test(bundle)) {
+      var alias = bundle.replace(extRe, '');
+      if (!has.call(aliases, alias) || aliases[alias].replace(extRe, '') === alias + '/index') {
+        aliases[alias] = bundle;
+      }
+    }
+
+    if (indexRe.test(bundle)) {
+      var iAlias = bundle.replace(indexRe, '');
+      if (!has.call(aliases, iAlias)) {
+        aliases[iAlias] = bundle;
+      }
+    }
+  };
+
+  require.register = require.define = function(bundle, fn) {
+    if (bundle && typeof bundle === 'object') {
+      for (var key in bundle) {
+        if (has.call(bundle, key)) {
+          require.register(key, bundle[key]);
+        }
+      }
+    } else {
+      modules[bundle] = fn;
+      delete cache[bundle];
+      addExtensions(bundle);
+    }
+  };
+
+  require.list = function() {
+    var list = [];
+    for (var item in modules) {
+      if (has.call(modules, item)) {
+        list.push(item);
+      }
+    }
+    return list;
+  };
+
+  var hmr = globals._hmr && new globals._hmr(_resolve, require, modules, cache);
+  require._cache = cache;
+  require.hmr = hmr && hmr.wrap;
+  require.brunch = true;
+  globals.require = require;
+})();
+
+(function() {
+var global = typeof window === 'undefined' ? this : window;
+var process;
+var __makeRelativeRequire = function(require, mappings, pref) {
+  var none = {};
+  var tryReq = function(name, pref) {
+    var val;
+    try {
+      val = require(pref + '/node_modules/' + name);
+      return val;
+    } catch (e) {
+      if (e.toString().indexOf('Cannot find module') === -1) {
+        throw e;
+      }
+
+      if (pref.indexOf('node_modules') !== -1) {
+        var s = pref.split('/');
+        var i = s.lastIndexOf('node_modules');
+        var newPref = s.slice(0, i).join('/');
+        return tryReq(name, newPref);
+      }
+    }
+    return none;
+  };
+  return function(name) {
+    if (name in mappings) name = mappings[name];
+    if (!name) return;
+    if (name[0] !== '.' && pref) {
+      var val = tryReq(name, pref);
+      if (val !== none) return val;
+    }
+    return require(name);
+  }
+};
+require.register("js/creative.js", function(exports, require, module) {
+"use strict";
+
+(function ($) {
+  "use strict"; // Start of use strict
+  // Smooth scrolling using jQuery easing
+
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top - 57
+        }, 1000, "easeInOutExpo");
+        return false;
+      }
+    }
+  }); // Closes responsive menu when a scroll trigger link is clicked
+
+  $('.js-scroll-trigger').click(function () {
+    $('.navbar-collapse').collapse('hide');
+  }); // Activate scrollspy to add active class to navbar items on scroll
+
+  $('body').scrollspy({
+    target: '#mainNav',
+    offset: 57
+  }); // Collapse Navbar
+
+  var navbarCollapse = function navbarCollapse() {
+    if ($("#mainNav").offset().top > 100) {
+      $("#mainNav").addClass("navbar-shrink");
+    } else {
+      $("#mainNav").removeClass("navbar-shrink");
+    }
+  }; // Collapse now if page is not at top
+
+
+  navbarCollapse(); // Collapse the navbar when page is scrolled
+
+  $(window).scroll(navbarCollapse); // Scroll reveal calls
+
+  window.sr = ScrollReveal();
+  sr.reveal('.sr-icons', {
+    duration: 600,
+    scale: 0.3,
+    distance: '0px'
+  }, 200);
+  sr.reveal('.sr-button', {
+    duration: 1000,
+    delay: 200
+  });
+  sr.reveal('.sr-contact', {
+    duration: 600,
+    scale: 0.3,
+    distance: '0px'
+  }, 300); // Magnific popup calls
+
+  $('.popup-gallery').magnificPopup({
+    delegate: 'a',
+    type: 'image',
+    tLoading: 'Loading image #%curr%...',
+    mainClass: 'mfp-img-mobile',
+    gallery: {
+      enabled: true,
+      navigateByImgClick: true,
+      preload: [0, 1]
+    },
+    image: {
+      tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
+    }
+  });
+})(jQuery); // End of use strict
+});
+
+;require.register("js/mantraretreat.js", function(exports, require, module) {
+"use strict";
+
+require("jquery");
+
+require("bootstrap");
+
+require("jquery.easing");
+
+require("scrollreveal");
+
+require("magnific-popup");
+
+require("@fortawesome/fontawesome-free");
+
+require("./creative.js");
+
+var donation_selected = false;
+var donation_amount = 0;
+
+var chkFrm = function chkFrm(form, action) {
+  var pp_link = 'https://paypal.me/ISKCONCommunications/';
+  var radios = form['reg_amt'];
+
+  for (i = 0; i < radios.length; i++) {
+    if (radios[i].checked) {
+      donation_selected = true;
+      donation_amount = radios[i].value;
+
+      if (donation_amount < 0) {
+        $('#step2').show();
+        $('#pp_button').hide();
+      } else {
+        pp_link += donation_amount;
+        $('#pp_button').show();
+        $('#step2').hide();
+      }
+    }
+  }
+
+  if (action == 'submit') {
+    window.open(pp_link, '_blank');
+    $('#step2').show();
+    $('#pp_reg').show();
+  }
+
+  return false;
+};
+
+var stepTwo = function stepTwo() {
+  var reg_link = 'https://docs.google.com/forms/d/e/1FAIpQLScaucbXHE-YRdjKmbgaLpfpXpzLw45wzzNpeRwco-sMe_vr6A/viewform?usp=pp_url';
+
+  if (!donation_selected) {
+    alert("Please select a registration Package.");
+  } else {
+    var params = '&entry.371804978=$' + donation_amount + '+USD';
+    reg_link += encodeURI(params);
+    window.open(reg_link);
+  }
+
+  return false;
+};
+
+console.log("mantraretreat.js loaded");
+});
+
+;require.alias("process/browser.js", "process");process = require('process');require.register("___globals___", function(exports, require, module) {
+  
+
+// Auto-loaded modules from config.npm.globals.
+window["$"] = require("jquery");
+window.jQuery = require("jquery");
+window.bootstrap = require("bootstrap");
+
+
+});})();require('___globals___');
+
+
+//# sourceMappingURL=mantraretreat.js.map
